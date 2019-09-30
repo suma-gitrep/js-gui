cdd584fdd492489dbb4a880e88366aecac8baec4
suma-gitrep <s537239@nwmissouri.edu>
2019-09-30 11:23:43 -0500

    made some changes

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const add = (x, y) => { return x + y }
+const avg = (x, y) => { 
+  
+  
+  return x*y }
 
 const validate = async (event) => {
   console.log(`triggered validate on ${event.target.id}`)
@@ -13,31 +16,27 @@ const validate = async (event) => {
 
 const updateWithAdd = async (event) => {
   document.querySelector('#result').innerHTML = ''
-  if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
+  if (document.querySelector('#itemsPerBag').checkValidity() && document.querySelector('#bags').checkValidity()) {
     const regex = /[^a-zA-Z_]/g
     const s = document.querySelector('#guest').value.replace(regex, '')
-    const i = parseInt(document.querySelector('#firstNumber').value)
-    const j = parseInt(document.querySelector('#secondNumber').value)
-    const ans = `${s}, your sum is ${add(i, j)}.`
+    const i = parseInt(document.querySelector('#itemsPerBag').value)
+    const j = parseInt(document.querySelector('#bags').value)
+    const final= avg(i,j);
+    
+    const ans = `${s}, your total number of items is  . ${final}`
+
     document.querySelector('#result').innerHTML = ans
   }
 }
 
-const updateWithJoke = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  const url = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
-  const response = await fetch(url)
-  const obj = await response.json()
-  const joke = obj.value.joke || 'No joke for you.'
-  document.querySelector('#result').innerHTML = joke
-}
+
 
 // delegate to dynamic elements (e.g. when testing)
 // focusout is like blur, but it bubbles up
 
 document.addEventListener('focusout', event => {
-  if ((event.target && event.target.id === 'firstNumber') ||
-    (event.target && event.target.id === 'secondNumber')) {
+  if ((event.target && event.target.id === 'itemsPerBag') ||
+    (event.target && event.target.id === 'bags')) {
     validate(event)
   }
 })
@@ -46,6 +45,3 @@ document.addEventListener('click', event => {
   if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
 })
 
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
-})
